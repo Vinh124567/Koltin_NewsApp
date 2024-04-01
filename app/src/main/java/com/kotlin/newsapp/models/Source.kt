1c9283639aa0d8c64ccb8e53c978d2e3c15d22ce
Vinh124567 <phungducvinh1245@gmail.com>
2024-04-02 00:36:49 +0700

    check fun favourite

--- app/src/main/java/com/kotlin/newsapp/models/Source.kt
+++ app/src/main/java/com/kotlin/newsapp/models/Source.kt
@@ -1,6 +1,14 @@
+//package com.kotlin.newsapp.models
+//
+//data class Source(
+//    val id: String?,
+//    val name: String?
+//)
 package com.kotlin.newsapp.models
 
 data class Source(
-    val id: String,
-    val name: String
-)
+    val id: String? = null,
+    val name: String? = null
+) {
+    constructor() : this("", "") // Thêm constructor không đối số
+}
